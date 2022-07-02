6dae183256670d2e58ab5b5eb0b46065c294599c
andrewsuds <andrewsuds@gmail.com>
2022-07-01 23:03:21 -0400

    Created Activity Page

---
+++ pages/activity.js
@@ -0,0 +1,10 @@
+import NavBar from "../components/NavBar";
+
+export default function ActivityPage() {
+  return (
+    <div>
+      <NavBar back={true} title="Activity" />
+      <div>All of activity...</div>
+    </div>
+  );
+}
