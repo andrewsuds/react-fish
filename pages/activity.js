958cb4ae4e293e5a06fc7774b6f50167c4bc3a33
andrewsuds <andrewsuds@gmail.com>
2022-07-03 00:23:19 -0400

    Changed Activity Page

--- pages/activity.js
+++ pages/activity.js
@@ -1,10 +1,74 @@
 import NavBar from "../components/NavBar";
+import Axios from "axios";
+import { useEffect, useState } from "react";
+import { BackendURL } from "../lib/BackendURL";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function ActivityPage() {
+  const [activity, setActivity] = useState([]);
+
+  useEffect(() => {
+    Axios.get(`${BackendURL}/profile/activity`).then((response) => {
+      setActivity(response.data);
+    });
+  }, []);
   return (
-    <div>
+    <div className="mb-[50px]">
       <NavBar back={true} title="Activity" />
-      <div>All of activity...</div>
+      {activity.map((value) => {
+        return (
+          <div
+            key={value.postid}
+            className="px-4 py-2 flex items-center border-t border-gray-200"
+          >
+            <div className="w-[35px] h-[35px] mr-3">
+              <Image
+                src={`${BackendURL}/images/ufc.jpg`}
+                className="rounded-full"
+                width={35}
+                height={35}
+                layout="fixed"
+              />
+            </div>
+            <div>
+              <Link href={`/${value.username}`}>
+                <span className="font-bold cursor-pointer hover:underline">
+                  {value.username}
+                </span>
+              </Link>
+
+              {value.type === "like" && <span>{" liked your "}</span>}
+              {value.type === "comment" && <span>{" commented on your "}</span>}
+
+              <Link href={`/${value.username}/post/${value.postid}`}>
+                <span className="font-bold cursor-pointer hover:underline">
+                  post
+                </span>
+              </Link>
+
+              <span>. </span>
+              {value.activitydate.days ? (
+                <span className="text-gray-500">
+                  {value.activitydate.days}d
+                </span>
+              ) : value.activitydate.hours ? (
+                <span className="text-gray-500">
+                  {value.activitydate.hours}h
+                </span>
+              ) : value.activitydate.minutes ? (
+                <span className="text-gray-500">
+                  {value.activitydate.minutes}m
+                </span>
+              ) : (
+                <span className="text-gray-500">
+                  {value.activitydate.seconds}s
+                </span>
+              )}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
