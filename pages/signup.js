92e78b53c9a4acd5df2c47ba182fd46705f959aa
andrewsuds <andrewsuds@gmail.com>
2022-07-18 21:57:42 -0400

    Revert "Changed Login and Signup Styling"
    This reverts commit dbe294bf0849406c0bf2dac1679aa21cbd245f11.

--- pages/signup.js
+++ pages/signup.js
@@ -44,7 +44,7 @@ export default function SignupPage() {
   };
 
   return (
-    <div className="max-w-sm mx-auto">
+    <div>
       <div className="top-0 sticky text-black backdrop-blur-lg bg-white/80 h-14 z-[1] flex items-center px-4 justify-center">
         <div
           className="absolute left-[16px] p-[7.5px] rounded-full hover:bg-gray-500/20 cursor-pointer"
